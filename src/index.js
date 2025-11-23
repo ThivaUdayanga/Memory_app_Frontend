0c23033dbe457b4d1ebb86fbce180ab93ddcc5fa
Thivanka <thivanka.wimalasena@gmail.com>
2025-11-24 00:25:20 +0530

    not working status

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 
+import { Provider } from 'react-redux'
+import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux'
+import { thunk } from 'redux-thunk'
+
+import reducers from './reducers'
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
